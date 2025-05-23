0102d728a52eded383ddd48e2713047f88ce2468
cyber-sec0 <91571849+cyber-sec0@users.noreply.github.com>
2025-05-23 02:35:47 +0000

    .

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-document.querySelectorAll(".wallet-address").forEach(function (element) { // Loop through each wallet Addresses element
+document.querySelectorAll('.wallet-address').forEach(function (element) {
+  // Loop through each wallet Addresses element
   element.onclick = function () {
     navigator.clipboard.writeText(element.textContent);
 
-    const copyMessage = document.querySelector("#copyMessage"); // Get the next sibling which is the copy message
+    const copyMessage = document.querySelector('#copyMessage'); // Get the next sibling which is the copy message
 
-    copyMessage.style.display = "block"; // Show copied message
-    copyMessage.style.top = this.getBoundingClientRect().top - document.body.getBoundingClientRect().top - copyMessage.offsetHeight + 65 + "px";
-    copyMessage.style.left = "37%";
+    copyMessage.style.display = 'block'; // Show copied message
+    copyMessage.style.top = this.getBoundingClientRect().top - document.body.getBoundingClientRect().top - copyMessage.offsetHeight + 65 + 'px';
+    copyMessage.style.left = '37%';
 
     setTimeout(function () {
-      document.getElementById("copyMessage").style.display = "none";
+      document.getElementById('copyMessage').style.display = 'none';
     }, 1000); // Hide message after 1 second
   };
 });
